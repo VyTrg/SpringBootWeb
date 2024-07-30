3e07f9ce8fd6ead90aae1f0cdaa01ed4793155b0
VyTrg <140966315+VyTrg@users.noreply.github.com>
2024-07-30 09:52:51 +0700

    commit

--- src/main/webapp/static/js/nhanVienDichVu.js
+++ src/main/webapp/static/js/nhanVienDichVu.js
@@ -1,13 +1,3 @@
 addEventListener("DOMContentLoaded", (event) => {
-	var suaButton = document.querySelectorAll(".js-update");
-	var tableModelUpdate = document.querySelector(".js-table-service");
-	tableModelUpdate.style.display = "none";
-
-	suaButton.forEach(button => {
-		button.addEventListener('click',handleSuaButtonClick)
-	})
 	
-	function handleSuaButtonClick(event) {
-		tableModelUpdate.style.display = "";
-	}
 });
